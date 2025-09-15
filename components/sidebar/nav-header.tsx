3c8ac4a93d8a9fe40484a7ea35c80d01cd4c2926
Kingdom <50469878+18682022127@users.noreply.github.com>
2025-09-15 11:42:11 +0800

    Update nav-header.tsx

--- components/sidebar/nav-header.tsx
+++ components/sidebar/nav-header.tsx
@@ -2,20 +2,6 @@
 
 
 
-import { ChevronsUpDown } from "lucide-react";
-import {
-  SidebarMenu,
-  SidebarMenuItem,
-  SidebarMenuButton,
-  useSidebar,
-} from "@/components/ui/sidebar";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
 
 export function NavHeader() {
   
